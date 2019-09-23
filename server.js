f4f8c277080d36b74b338483fa0de55734bf5803
dbilderback <dbilderback@gmail.com>
2019-09-23 17:00:28 -0400

    implemented redux, user registration, system alerts

--- server.js
+++ server.js
@@ -1,24 +1,28 @@
-const express = require('express');
-const connectDB = require('./config/db');
-
+const express = require("express");
+const connectDB = require("./config/db");
+const cors = require("cors");
 const app = express();
 
 //Connect DB
 connectDB();
 
 // Init Middleware
-app.use(express.json({
-  extended: false
-}));
+app.use(
+  express.json({
+    extended: false
+  })
+);
 
-app.get('/', (req, res) => res.send('API Running'));
+app.use(cors());
+
+app.get("/", (req, res) => res.send("API Running"));
 
 //Define Routes
-app.use('/api/users', require('./routes/api/users'));
-app.use('/api/posts', require('./routes/api/posts'));
-app.use('/api/profile', require('./routes/api/profile'));
-app.use('/api/auth', require('./routes/api/auth'));
+app.use("/api/users", require("./routes/api/users"));
+app.use("/api/posts", require("./routes/api/posts"));
+app.use("/api/profile", require("./routes/api/profile"));
+app.use("/api/auth", require("./routes/api/auth"));
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => console.log('Server started on port ${PORT}'));
+app.listen(PORT, () => console.log("Server started on port ${PORT}"));
